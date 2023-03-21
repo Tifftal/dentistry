8cf9ec06fafb4f746017c1b7f5b4607d4aadfd1f
VarTy <talankina.v@mail.ru>
2023-03-21 09:48:34 +0300

    VARAVARA

---
+++ client/src/Components/Navbar/navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navbar = () => {
+    return (
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#"></a>
+            <button type="button" className="btn btn-outline-success">Иконка (всплывающее меню)</button>
+
+            <div className="collapse navbar-collapse" id="navbarText">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Услуги</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Цены</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">О нас</a>
+                    </li>
+                </ul>
+                <div className="text-center">
+                    <img src="/Users/vaarty/Desktop/dentistry/client/src/IMG/Новый проект-2.png" />
+                </div>
+                <span className="navbar-text">
+                    Navbar text with an inline element
+                </span>
+
+            </div>
+
+        </nav>
+
+    );
+}
+
+export default Navbar;

b9fd5eb2aceb107665a0791064b4aed34c4c30b4
VarTy <talankina.v@mail.ru>
2023-03-21 11:51:51 +0300

    Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import Main from './Components/Main/main';
 import './App.css';
 import Navbar from './Components/Navbar/navbar';
+import Footer from './Components/Footer/footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Main />
+      <Footer />
     </div>
   );
 }
--- client/src/Components/Footer/footer.js
+++ client/src/Components/Footer/footer.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './footer.css'
 
 const Footer = () => {
     return (
-        <footer className="footer">
-        <div className="container">
-            <span className="text-muted">Place sticky footer content here.</span>
+        <div>
+            <article className="columns-4">
+                <div>
+                    <p className="footer_left">
+                        О возможных противопоказаниях проконсультируйтесь с нашими специалистами</p>
+                    <ul class="social-icons">
+                        <li><a class="social-icon-whatsapp" href="https://vk.com/im?peers=409551839" title="..." target="_blank" rel="noopener"></a></li>
+                        <li><a class="social-icon-vk" href="#" title="..." target="_blank" rel="noopener"></a></li>
+                        <li><a class="social-icon-telegram" href="#" title="..." target="_blank" rel="noopener"></a></li>
+                    </ul>
+                </div>
+                <p className="footer">To call Bowie a transitional figure in rock history is less a judgment than a job description. Every niche he ever found was on a cusp, and he was at home nowhere else—certainly not in the unmoneyed London suburb where his childhood was as dingy as his adult life would be glitzy. While this born dabbler’s favourite pose was that of a Great Artist beguiled by rock’s possibilities as a vehicle, in truth he was more a rocker drawn to artiness because it worked better than any other pose he had tried (not that he was not eclectic—he admired Anthony Newley and Jacques Brel and studied mime with Lindsay Kemp).</p>
+                <p className="footer">David Bowie, original name David Robert Jones, (born January 8, 1947, London, England—died January 10, 2016, New York, New York, U.S.), British singer, songwriter, and actor who was most prominent in the 1970s and best known for his shifting personae and musical genre hopping.</p>
+                <p className="footer_right">David Bowie, original name David Robert Jones, (born January 8, 1947, London, England—died January 10, 2016, New York, New York, U.S.), British singer, songwriter, and actor who was most prominent in the 1970s and best known for his shifting personae and musical genre hopping.</p>
+            </article>
         </div>
-        </footer>
     );
 }
 

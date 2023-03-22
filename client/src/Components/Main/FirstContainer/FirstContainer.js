2541c12685d9472a942d99bc97a458d8806ffc1a
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-03-22 21:47:02 +0300

    changes

--- client/src/Components/Main/FirstContainer/FirstContainer.js
+++ client/src/Components/Main/FirstContainer/FirstContainer.js
@@ -1,23 +1,37 @@
 import React from "react";
+import { useState } from "react";
+import PortalReactDOM from 'react-dom'
+import Note from "../../Popup/Note/Note";
 import "./FirstContainer.css"
 const FirstContainer = () => {
+    const [isOpen, setIsOpen] = useState('false');
+
+    const HandleOpenNote = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const HandleCloseNote = () => {
+        setIsOpen(!isOpen)
+    }
     return (
-    <div className="firstComponent">
-        <div className="WelcomeContainer">
-            <h1>
-            Мы дарим вам бесплатную
-            </h1>
-            <h1>
-            чистку зубов
-            </h1>
-            <p>
-                Наши врачи - профессионалы своего дела
-            </p>
-            <button>
-                Записаться на прием
-            </button>
+        <div className="firstComponent">
+            {PortalReactDOM.createPortal(
+                <Note isOpen={isOpen} onClose={HandleCloseNote} />,
+                document.body
+            )}
+            <div className="WelcomeContainer">
+                <h1>
+                    Мы дарим вам бесплатную
+                </h1>
+                <h1>
+                    чистку зубов
+                </h1>
+                <p>
+                    Наши врачи - профессионалы своего дела
+                </p>
+                <button onClick={HandleOpenNote}>Записаться на прием</button>
+            </div>
         </div>
-    </div>
     )
 }
 

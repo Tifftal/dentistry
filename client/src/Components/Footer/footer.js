46cf4df21c61d5d307ede1342880dfe75f0bbdc6
VarTy <talankina.v@mail.ru>
2023-03-29 21:41:19 +0300

    footer+note

--- client/src/Components/Footer/footer.js
+++ client/src/Components/Footer/footer.js
@@ -10,11 +10,18 @@ const Footer = () => {
                 <div>
                     <p className="footer_left">
                         О возможных противопоказаниях проконсультируйтесь с нашими специалистами</p>
-                    <ul className="social-icons">
-                        <li><a className="social-icon-whatsapp" href="#" title="..." target="_blank" rel="noopener"></a></li>
-                        <li><a className="social-icon-vk" href="#" title="..." target="_blank" rel="noopener"></a></li>
-                        <li><a className="social-icon-telegram" href="#" title="..." target="_blank" rel="noopener"></a></li>
-                    </ul>
+                    <div className='socialBarFooter'>
+                        
+                        <div className='scBtnFooter'>
+                            <a href="#"><img src="../IMG/free-icon-whatsapp-3670302.png" width="90%" height="90%" /></a>
+                        </div>
+                        <div className='scBtnFooter'>
+                            <a href="#"><img src="../IMG/free-icon-vkontakte-4494490.png" width="90%" height="90%" /></a>
+                        </div>
+                        <div className='scBtnFooter'>
+                            <a href="#"><img src="../IMG/free-icon-telegram-2111710.png" width="90%" height="90%" /></a>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="footer_menu">
@@ -37,10 +44,10 @@ const Footer = () => {
                         <input type="text" name="name" placeholder="Телефон" id="name" required />
                         <input type="text" name="name" placeholder="Причина обращения" id="name" required />
                         <div className='FormDiv'>
-                            <input type="checkbox" name="intensive-courses" id="courses" checked />
+                            <input type="checkbox" name="intensive-courses" id="courses" unchecked />
                             <label for="courses">
                                 Выражаю свое согласие на обработку персональных данных в соответствии с <a href="#">Федеральном законом "О персональных данных" от 27.07.2006 № 152-ФЗ</a>, с условиями обработки персональных данных ознакомлен(а).
-                                </label>
+                            </label>
                         </div>
                         <button type="submit">Отправить заявку</button>
                     </form>
--- client/src/Components/Popup/Note/Note.js
+++ client/src/Components/Popup/Note/Note.js
@@ -6,7 +6,18 @@ function Modal(props) {
     <div className="modal">
       <div className="modal-content">
         <span className="close" onClick={props.onClose}>&times;</span>
-        <p>Modal content goes here</p>
+        <form className='formNote'>
+          <input type="text" name="name" placeholder="Имя" id="name" required />
+          <input type="text" name="name" placeholder="Телефон" id="name" required />
+          <input type="text" name="name" placeholder="Причина обращения" id="name" required />
+          <div className='formDivNote'>
+            <input type="checkbox" name="intensive-courses" id="courses" unchecked />
+            <label for="courses">
+              Выражаю свое согласие на обработку персональных данных в соответствии с <a href="#">Федеральном законом "О персональных данных" от 27.07.2006 № 152-ФЗ</a>, с условиями обработки персональных данных ознакомлен(а).
+            </label>
+          </div>
+          <button type="submit">Отправить заявку</button>
+        </form>
       </div>
     </div>
   );

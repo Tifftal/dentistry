c8b49814fa27bafc0a261997f0b1855be318b4d4
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-04-04 13:39:36 +0300

    commit

--- client/src/Components/About/about.js
+++ client/src/Components/About/about.js
@@ -1,9 +1,34 @@
 import react from "react"
+import "./about.css"
 
 const About = () => {
     return (
-        <div>
-            Информация о клинике
+        <div className="background">
+            <div className="titleDoc">
+                    <h1>О нас</h1>
+                </div>
+                <div>
+                    <ul class="breadcrumb">
+                        <a href="/">Главная</a> - О нас
+                    </ul>
+                </div>
+            <div className="Slider">
+
+            </div>
+
+            <div className="about">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum fusce ut placerat orci nulla pellentesque dignissim. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Volutpat blandit aliquam etiam erat velit scelerisque in. Faucibus scelerisque eleifend donec pretium vulputate sapien. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Leo urna molestie at elementum. Tincidunt nunc pulvinar sapien et. Turpis egestas integer eget aliquet nibh praesent tristique. Habitant morbi tristique senectus et netus et malesuada fames. Laoreet suspendisse interdum consectetur libero id. Maecenas sed enim ut sem viverra aliquet eget. Sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Sollicitudin tempor id eu nisl. Egestas fringilla phasellus faucibus scelerisque eleifend donec. Integer enim neque volutpat ac tincidunt.
+
+                    Ornare arcu dui vivamus arcu felis bibendum ut tristique. Volutpat consequat mauris nunc congue nisi vitae suscipit. Sagittis id consectetur purus ut faucibus pulvinar elementum. Est velit egestas dui id ornare arcu odio ut sem. Et malesuada fames ac turpis. Nulla facilisi etiam dignissim diam quis enim lobortis. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. In eu mi bibendum neque egestas congue quisque egestas diam. Id diam maecenas ultricies mi. Imperdiet massa tincidunt nunc pulvinar. Est sit amet facilisis magna etiam tempor orci eu.
+
+                    Tellus in hac habitasse platea dictumst vestibulum rhoncus. Commodo viverra maecenas accumsan lacus vel facilisis volutpat. Odio ut sem nulla pharetra diam. Arcu bibendum at varius vel. Semper risus in hendrerit gravida rutrum quisque non tellus. Nibh cras pulvinar mattis nunc. Adipiscing commodo elit at imperdiet dui. Faucibus a pellentesque sit amet porttitor eget dolor morbi non. Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Massa vitae tortor condimentum lacinia quis vel eros. Sodales ut eu sem integer vitae justo. Libero id faucibus nisl tincidunt eget. Integer feugiat scelerisque varius morbi enim. Leo vel orci porta non pulvinar neque laoreet. Vitae proin sagittis nisl rhoncus. Interdum posuere lorem ipsum dolor sit amet.
+
+                    Viverra maecenas accumsan lacus vel facilisis volutpat est velit. Est placerat in egestas erat imperdiet. Odio aenean sed adipiscing diam donec adipiscing tristique. Viverra aliquet eget sit amet tellus cras. Id faucibus nisl tincidunt eget. Vulputate ut pharetra sit amet aliquam id diam. Duis at consectetur lorem donec massa. Quis risus sed vulputate odio ut enim. Feugiat nibh sed pulvinar proin. Dictum varius duis at consectetur. Nunc lobortis mattis aliquam faucibus purus in. Mattis rhoncus urna neque viverra justo nec ultrices dui. Neque volutpat ac tincidunt vitae semper. Nunc sed velit dignissim sodales ut. Dolor sit amet consectetur adipiscing elit duis. Erat velit scelerisque in dictum. Velit euismod in pellentesque massa placerat duis ultricies lacus. Sed risus pretium quam vulputate dignissim suspendisse in est. Tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus.
+
+                    Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Scelerisque purus semper eget duis at tellus at urna condimentum. Tristique risus nec feugiat in fermentum posuere urna nec. Ultricies mi eget mauris pharetra et ultrices neque ornare. Mi proin sed libero enim sed faucibus turpis in. Tellus mauris a diam maecenas sed enim. Lobortis elementum nibh tellus molestie nunc. Arcu risus quis varius quam quisque id. Facilisis volutpat est velit egestas dui id ornare arcu odio. Turpis nunc eget lorem dolor sed viverra ipsum nunc. Sit amet facilisis magna etiam tempor orci. Sed velit dignissim sodales ut eu sem integer vitae justo. Risus feugiat in ante metus dictum. Diam quam nulla porttitor massa id neque aliquam. Nunc lobortis mattis aliquam faucibus. Et ultrices neque ornare aenean euismod elementum nisi. Metus dictum at tempor commodo.
+                </p>
+            </div>
         </div>
     )
 }

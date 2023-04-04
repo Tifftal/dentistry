b0eb12cf90b705c91aa6376ba57948601bc3debe
VarTy <talankina.v@mail.ru>
2023-04-04 13:39:54 +0300

    portfolio

---
+++ client/src/Components/Portfolio/portfolio.js
@@ -0,0 +1,12 @@
+import react from "react"
+import './portfolio.css'
+
+const Portfolio = () => {
+    return (
+        <div className="backgr_port">
+            
+        </div>
+    )
+}
+
+export default Portfolio

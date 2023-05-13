4322b2183ea594458a2a4d1584c7b5854aeb0a2d
VarTy <talankina.v@mail.ru>
2023-05-14 00:18:01 +0300

    dd

--- client/src/Components/Main/SecondContainer/SecondContainer.js
+++ client/src/Components/Main/SecondContainer/SecondContainer.js
@@ -22,7 +22,7 @@ const SecondContainer = () => {
             <div className="box2">
                 <img src="../../IMG/DSC02687-2.jpg" />
                 <h3>
-                    Куашева Фатима Магометовна
+                    
                 </h3>
                 <p>
                     Терапевт
@@ -49,7 +49,7 @@ const SecondContainer = () => {
             <div className="box5">
                 <img src="../../IMG/DSC02705-2.jpg" />
                 <h3>
-                    Иванов Иван Иванович
+                Куашева Фатима Магометовна
                 </h3>
                 <p>
                     Терапевт

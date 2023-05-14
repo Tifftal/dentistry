12d46f94c0d10b3c857a59b7b06a36123627421f
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-05-15 00:05:56 +0300

    Верстка финалочка

--- client/src/Components/Main/Way/way.js
+++ client/src/Components/Main/Way/way.js
@@ -5,15 +5,13 @@ const Way = () => {
     return (
         <div className="contact">
             <div className="titleContact">
-                <h1>
+                <h1 id="map">
                     Адрес клиники
                 </h1>
             </div>
 
             <div className="map">
-                {/* <a className="maptitle" href="https://yandex.ru/maps/org/formula_ulybki/1215012496/?utm_medium=mapframe&utm_source=maps">Формула улыбки</a>
-                <a className="maptext" href="https://yandex.ru/maps/35/krasnodar/category/dental_clinic/184106132/?utm_medium=mapframe&utm_source=maps">Стоматологическая клиника в Краснодаре</a> */}
-                <iframe src="https://yandex.ru/map-widget/v1/?ll=39.009379%2C45.021054&mode=poi&poi%5Bpoint%5D=39.009469%2C45.020941&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D1215012496&z=18.62" width="95%" height="500px" frameborder="1" allowFullScreen="true"></iframe>
+                <iframe src="https://yandex.ru/map-widget/v1/?ll=39.009379%2C45.021054&mode=poi&poi%5Bpoint%5D=39.009469%2C45.020941&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D1215012496&z=18.62" title="map" width="95%" height="500px" frameBorder="1" allowFullScreen={true}></iframe>
             </div>
         </div>
     )

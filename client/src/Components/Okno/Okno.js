dca96449dbc212a97131c7eec15bceca437ebbbf
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-05-17 23:16:40 +0300

    ссылка на обработку персональных данных и прайс

--- client/src/Components/Okno/Okno.js
+++ client/src/Components/Okno/Okno.js
@@ -12,7 +12,7 @@ function Okno(props) {
           <input type="text" name="name" placeholder="Телефон" id="name" required />
           <input type="text" name="name" placeholder="Причина обращения" id="name" required />
           <button type="submit">Отправить заявку</button>
-          <label>Нажимая на кнопку "Отправить заявку", Вы выражаете свое согласие на <a href="http://localhost:3000/#:~:text=%D0%A4%D0%B5%D0%B4%D0%B5%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%BC%20%D0%B7%D0%B0%D0%BA%D0%BE%D0%BD%D0%BE%D0%BC%20%22%D0%9E%20%D0%BF%D0%B5%D1%80%D1%81%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85%20%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85%22%20%D0%BE%D1%82%2027.07.2006%20%E2%84%96%20152%2D%D0%A4%D0%97">обработку персональных данных</a></label>
+          <label>Нажимая на кнопку "Отправить заявку", Вы выражаете свое согласие на <a href="https://www.consultant.ru/document/cons_doc_LAW_61801/">обработку персональных данных</a></label>
         </form>
       </div>
     </div>

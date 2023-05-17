d5bb27af9d2e7e36eb1f01b9a8716bad7f51cf32
VarTy <talankina.v@mail.ru>
2023-05-17 23:21:43 +0300

    чч

---
+++ client/src/Components/Таценко/Tacenko.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Tacenko.css"
+
+const Tacenko = () => {
+    return (
+        <div className="tacenko">
+            Таценко
+        </div>
+    )
+}
+
+export default Tacenko;

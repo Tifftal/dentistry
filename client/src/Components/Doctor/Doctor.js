7b74600d4515f614f0ce6ef7667c73d05c63244b
VarTy <talankina.v@mail.ru>
2023-05-19 16:53:11 +0300

    орор

--- client/src/Components/Doctor/Doctor.js
+++ client/src/Components/Doctor/Doctor.js
@@ -1,22 +1,36 @@
 import React from "react";
 import Doc from "../../Store/Doctors";
 import { observer } from "mobx-react-lite";
+import './Doctor.css';
 
 const Doctor = observer(() => {
     return (
         <div className="doctorContainer">
-            <div>
-                <img src={Doc.img} />
-                <p>
-                    {Doc.surname} {Doc.name} {Doc.patronimic}
-                </p>
-                {
-                    Doc.docs?.map(docs => (
-                        <a href={docs} download={docs}><button>Документы</button></a>
-                    ))
-                }
+            <h1>
+                {Doc.surname} {Doc.name} {Doc.patronimic}
+            </h1>
+            <div className="DoctorCard">
+                <div>
+                    <img src={Doc.img} />
+                </div>
+                <div>
+                    <h2>Образование</h2>
+                    <p>
+                        {Doc.education}
+                    </p>
+                    <h2>Квалификация</h2>
+                    <p>
+                        {Doc.qualification}
+                    </p>
+                    <h2>О стоматологе</h2>
+                    <p>
+                        {Doc.about}
+                    </p>
+                    <button>Документы</button>
+                </div>
+
             </div>
-        </div>
+        </div >
     )
 });
 

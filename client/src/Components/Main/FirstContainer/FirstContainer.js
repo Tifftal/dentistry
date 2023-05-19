553e7dc3d668893103e9f9f8ba59aa446d7c2949
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-05-20 01:20:02 +0300

    Axios post request

--- client/src/Components/Main/FirstContainer/FirstContainer.js
+++ client/src/Components/Main/FirstContainer/FirstContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 import "./FirstContainer.css"
+import axios from "axios";
 
 const FirstContainer = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,16 +14,39 @@ const FirstContainer = () => {
     const HandleCloseNote = () => {
         setIsOpen(false)
     }
+
+    const handleSubmitForm = (event) => {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const phone = document.getElementById('phone').value;
+
+        const data = {
+            name: name,
+            phone: phone,
+            timestamp: new Date().toISOString(),
+        }
+
+        console.log(data);
+
+        axios.post('api?', data)
+            .then(function (response){
+                console.log(response);
+            }) 
+            .catch(function (error) {
+                console.log(error);
+            }) 
+    }
+
     return (
         <div className="firstComponent">
             {isOpen && (
                 <div className="modal">
                     <div className="modal-content">
                         <span className="close" onClick={HandleCloseNote}>&times;</span>
-                        <form className='formNote'>
-                            <input type="text" name="name" placeholder="Имя" id="name" required />
-                            <input type="text" name="name" placeholder="Телефон" id="name" required />
-                            <input type="text" name="name" placeholder="Причина обращения" id="name" required />
+                        <form className='formNote' onSubmit={handleSubmitForm}>
+                            <input type="text" name="name" placeholder="Имя" id="name" required pattern="[a-zA-Zа-яА-ЯЁё\s]+" title="Введите имя на русском или английском" />
+                            <input type="text" name="name" placeholder="Телефон" id="name" required pattern="^\+?[1-9]\d{1,10}$" title="Введите корректный номер телефона" />
                             <button type="submit">Отправить заявку</button>
                             <label>Нажимая на кнопку "Отправить заявку", Вы выражаете свое согласие на <a href="https://www.consultant.ru/document/cons_doc_LAW_61801/">обработку персональных данных</a></label>
                         </form>
--- client/src/Components/Okno/Okno.js
+++ client/src/Components/Okno/Okno.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import './Okno.css'
+import axios from "axios";
 
 function Okno(props) {
 
-  return (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={props.onClose}>&times;</span>
-        <form className='formNote'>
-          <input type="text" name="name" placeholder="Имя" id="name" required />
-          <input type="text" name="name" placeholder="Телефон" id="name" required />
-          <input type="text" name="name" placeholder="Причина обращения" id="name" required />
-          <button type="submit">Отправить заявку</button>
-          <label>Нажимая на кнопку "Отправить заявку", Вы выражаете свое согласие на <a href="https://www.consultant.ru/document/cons_doc_LAW_61801/">обработку персональных данных</a></label>
-        </form>
-      </div>
-    </div>
-  );
+    const handleSubmitForm = (event) => {
+        event.preventDefault(); 
+
+        const name = document.getElementById('name').value;
+        const phone = document.getElementById('phone').value;
+
+        const data = {
+            name: name,
+            phone: phone,
+            timestamp: new Date().toISOString(),
+        };
+
+        console.log(data);
+
+        axios.post('api?', data)
+            .then(function (response) {
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    return (
+        <div className="modal">
+            <div className="modal-content">
+                <span className="close" onClick={props.onClose}>&times;</span>
+                <form className='formNote' onSubmit={handleSubmitForm}>
+                    <input type="text" name="name" placeholder="Имя" id="name" required pattern="[a-zA-Zа-яА-ЯЁё\s]+" title="Введите имя на русском или английском" />
+                    <input type="text" name="phone" placeholder="Телефон" id="phone" required pattern="^\+?[1-9]\d{1,10}$" title="Введите корректный номер телефона" />
+                    <button type="submit">Отправить заявку</button>
+                    <label>Нажимая на кнопку "Отправить заявку", Вы выражаете свое согласие на <a href="https://www.consultant.ru/document/cons_doc_LAW_61801/">обработку персональных данных</a></label>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Okno;

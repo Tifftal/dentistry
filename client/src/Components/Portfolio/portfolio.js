d70c2514ee9924a99459f8756abd517c248ac8b7
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-05-24 06:37:06 +0300

    commit

--- client/src/Components/Portfolio/portfolio.js
+++ client/src/Components/Portfolio/portfolio.js
@@ -1,7 +1,28 @@
-import react from "react"
-import './portfolio.css'
+import React, {useState, useEffect} from "react";
+import './portfolio.css';
+import axios from "axios";
 
 const Portfolio = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetchExistingImages();
+    }, [])
+
+    const fetchExistingImages = async () => {
+        try {
+            const response = await axios.get("http://localhost:8082/work/get");
+            // const data = response.data.map((item) => ({
+            //   ...item,
+            //   fileBefore: Uint8Array.from(item.fileBefore, (c) => c.charCodeAt(0)),
+            //   fileAfter: Uint8Array.from(item.fileAfter, (c) => c.charCodeAt(0)),
+            // }));
+            setData(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className="backgrPort">
             <div className="portfolio">
@@ -15,26 +36,32 @@ const Portfolio = () => {
                         <li>Портфолио</li>
                     </ul>
                 </div>
-                <div className="clinicalCase">
-                    <div className="caseContainer">
-                        <div className="case">
-                            <img src="../../IMG/protezirovanie_zubov_foto_do_i_posle_7.jpg" />
+                {data.map(image => (
+                    <div className="clinicalCase">
+                        <div className="caseContainer">
+                            <div className="case">
+                                {/* <img src="../../IMG/protezirovanie_zubov_foto_do_i_posle_7.jpg" /> */}
+                                <img src={`data:image/jpg;base64,` + image.fileBefore} alt={image.title} />
+                            </div>
+                            <div className="case">
+                                {/* <img src="../../IMG/protezirovanie_zubov_foto_do_i_posle_7-2.jpg" /> */}
+                                <img src={`data:image/jpg;base64,` + image.fileAfter} alt={image.title} />
+                            </div>
                         </div>
                         <div className="case">
-                            <img src="../../IMG/protezirovanie_zubov_foto_do_i_posle_7-2.jpg" />
+                            <h3>
+                                {image.title}
+                            </h3>
+                            <p>
+                                {/* Жалобы: боль в зубе при надкусывании.
+                                В другой клинике сказали, что необходимо удаление, т. к большая киста.
+                                Была удалена не корректная пломба, извлечён металлический штифт. В итоге спасли зуб, киста зажила. */}
+                                {image.content}
+                            </p>
                         </div>
                     </div>
-                    <div className="case">
-                        <h3>
-                            Клинический случай №1
-                        </h3>
-                        <p>
-                            Жалобы: боль в зубе при надкусывании.
-                            В другой клинике сказали, что необходимо удаление, т. к большая киста.
-                            Была удалена не корректная пломба, извлечён металлический штифт. В итоге спасли зуб, киста зажила.
-                        </p>
-                    </div>
-                </div>
+                ))
+                }
             </div>
         </div>
     )

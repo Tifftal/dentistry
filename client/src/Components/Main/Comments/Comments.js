7062637d85994cc5ad50ec80100c4391fcc35b35
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-05-26 00:34:12 +0300

    asdad

--- client/src/Components/Main/Comments/Comments.js
+++ client/src/Components/Main/Comments/Comments.js
@@ -38,9 +38,6 @@ const Comments = () => {
         const year = date.getFullYear();
         const month = m[String(date.getMonth() + 1).padStart(2, '0')];
         const day = String(date.getDate()).padStart(2, '0');
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
 
         return `${day} ${month} ${year} г.`;
     };

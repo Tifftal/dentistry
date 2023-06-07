24dbe9f23939b2460cbf17a415c1a9de2134952a
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-06-07 13:11:56 +0300

    zalito

--- client/src/Components/Comment/Comment.js
+++ client/src/Components/Comment/Comment.js
@@ -39,7 +39,7 @@ function Comment(props) {
         <form className='formNote' onSubmit={handleSubmitForm}>
           <input type="text" name="name" placeholder="Имя" id="name" required pattern="[a-zA-Zа-яА-ЯЁё\s]+" title="Введите имя на русском или английском" />
           <input type="text" name="phone" placeholder="Номер телефона" id="number" required pattern="^\+?[1-9]\d{1,10}$" title="Введите корректный номер телефона" />
-          <input type="text" name="comment" placeholder="Отзыв" id="comment" required />
+          <textarea type="text" name="comment" placeholder="Отзыв" id="comment" required />
           <button type="submit">Отправить отзыв</button>
           <label>Нажимая на кнопку "Отправить отзыв", Вы выражаете свое согласие на <a href="https://www.consultant.ru/document/cons_doc_LAW_61801/">обработку персональных данных</a></label>
         </form>

f078a693f1027017c0dd040b72f2affaf261787a
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-06-29 22:08:55 +0300

    New order

--- client/src/Components/AdminPanel/Pictures/Pictures.js
+++ client/src/Components/AdminPanel/Pictures/Pictures.js
@@ -1,160 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-
-// import "./Pictures.css";
-
-// const Pictures = (props) => {
-//     const [data, setData] = useState([])
-
-//     const fetchExistingImages = async () => {
-//         try {
-//             const response = await axios.get("https://formulaulybki.ru/api/work/get");
-//             const data = response.data.map((item) => ({
-//                 ...item,
-//                 fileBefore: item.fileBefore,
-//                 fileAfter: item.fileAfter,
-//             }));
-//             setData(data);
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     };
-
-//     useEffect(() => {
-//         fetchExistingImages();
-//     }, [fetchExistingImages])
-
-//     const handleImageUpload = async (event) => {
-//         event.preventDefault();
-
-//         const fileBeforeInput = document.getElementById('beforeImage');
-//         const fileAfterInput = document.getElementById('afterImage');
-
-//         const fileBefore = fileBeforeInput.files[0];
-//         const fileAfter = fileAfterInput.files[0];
-
-//         const readerBefore = new FileReader();
-//         const readerAfter = new FileReader();
-
-//         readerBefore.onload = function () {
-//             const fileBeforeData = readerBefore.result;
-
-//             readerAfter.onload = function () {
-//                 const fileAfterData = readerAfter.result;
-
-//                 const title = document.getElementById('title').value;
-//                 const content = document.getElementById('description').value;
-
-//                 const data = {
-//                     fileBefore: fileBeforeData,
-//                     fileAfter: fileAfterData,
-//                     title: title,
-//                     content: content,
-//                 };
-
-//                 console.log(data);
-
-//                 axios
-//                     .post("https://formulaulybki.ru/api/work/post", data)
-//                     .then(function (response) {
-//                         console.log(response.data);
-//                     })
-//                     .catch(function (error) {
-//                         console.log(error);
-//                     });
-//             };
-
-//             readerAfter.readAsDataURL(fileAfter);
-//         };
-
-//         readerBefore.readAsDataURL(fileBefore);
-//         fetchExistingImages();
-//     };
-
-
-
-//     const handleImageDelete = async (image) => {
-//         try {
-//             await axios.post(`https://formulaulybki.ru/api/work/delete/${image.id}`);
-//             fetchExistingImages();
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     };
-
-//     return (
-//         <div className="DoPosle">
-//             <button onClick={() => { props.setBtn('') }} className="back-Btn">Назад</button>
-//             <h1 style={{color:"rgba(0,0,0,0.6)"}}>Загрузка</h1>
-//             <form onSubmit={handleImageUpload} className="upload-image">
-//                 <label htmlFor="beforeImage">Изображение ДО:</label>
-//                 <input className="fileInput"
-//                     type="file"
-//                     id="beforeImage"
-//                     accept="image/*"
-//                     required
-//                 />
-//                 <label htmlFor="afterImage">Изображение После:</label>
-//                 <input className="fileInput"
-//                     type="file"
-//                     id="afterImage"
-//                     accept="image/*"
-//                     required
-//                 />
-//                 <input className="textInput"
-//                     type="text"
-//                     id="title"
-//                     placeholder="Заголовок"
-//                     required
-//                 />
-//                 <textarea className="areaInput"
-//                     id="description"
-//                     placeholder="Описание"
-//                     required
-//                 />
-//                 <button type="submit">Отправить</button>
-//             </form>
-//             <div className="Table">
-//                 <table className="existing-images">
-//                     <thead>
-//                         <tr>
-//                             <th>Заголовок</th>
-//                             <th>Описание</th>
-//                             <th className="image-cont">Картинка ДО</th>
-//                             <th className="image-cont">Картинка ПОСЛЕ</th>
-//                             <th>Действие</th>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                         {data.map((image) => (
-//                             <tr key={image.id}>
-//                                 <td>{image.title}</td>
-//                                 <td style={{wordBreak:"break-all"}}>{image.content}</td>
-//                                 <td className="image-cont">
-//                                     <img src={image.fileBefore} alt={image.title} className="image" />
-//                                 </td>
-//                                 <td className="image-cont">
-//                                     <img src={image.fileAfter} alt={image.title} className="image" />
-//                                 </td>
-//                                 <td>
-//                                     <button
-//                                         onClick={() => handleImageDelete(image)}
-//                                         className="delete-button"
-//                                     >
-//                                         Удалить
-//                                     </button>
-//                                 </td>
-//                             </tr>
-//                         ))}
-//                     </tbody>
-//                 </table>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Pictures;
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -171,6 +14,7 @@ const Pictures = (props) => {
     try {
       const response = await axios.get("https://formulaulybki.ru/api/work/get");
       setData(response.data);
+      console.log(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -264,6 +108,22 @@ const Pictures = (props) => {
     }
   };
 
+  const orderUp = async (id) => {
+    try {
+      await axios.post(`https://formulaulybki.ru//api/move/up/${id}`);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  const orderDown = async (id) => {
+    try {
+      await axios.post(`https://formulaulybki.ru//api/move/down/${id}`);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="DoPosle">
       <button onClick={() => props.setBtn("")} className="back-Btn">
@@ -315,6 +175,12 @@ const Pictures = (props) => {
           <tbody>
             {data.map((image) => (
               <tr key={image.id}>
+                <td>
+                  <div className="Arrows">
+                    <button onClick={()=>{orderUp(image.id)}}>↑</button>
+                    <button onClick={()=>{orderDown(image.id)}}>↓</button>
+                  </div>
+                </td>
                 <td>{image.title}</td>
                 <td style={{ wordBreak: "break-all" }}>{image.content}</td>
                 <td className="image-cont">

c18edeefad604a28f45425b46ff6cca129034c47
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-06-30 22:12:55 +0300

    фикс фикса

--- client/src/Components/Services/services.js
+++ client/src/Components/Services/services.js
@@ -61,91 +61,86 @@ const Services = () => {
                         <tbody>
                             <tr>
                                 <td className="col1">1.</td>
-                                <td className="col2">Консультация глав-врача.</td>
+                                <td className="col2">Консультация глав-врача</td>
                                 <td className="col3">1500 ₽</td>
                             </tr>
                             <tr>
                                 <td className="col1">2.</td>
-                                <td className="col2">Консультационные услуги.</td>
-                                <td className="col3">1000 ₽</td>
+                                <td className="col2">Консультационные услуги</td>
+                                <td className="col3">от 1000 ₽</td>
                             </tr>
                             <tr>
                                 <td className="col1">3.</td>
-                                <td className="col2">Консультационные услуги</td>
-                                <td className="col3">1000 ₽</td>
-                            </tr>
-                            <tr>
-                                <td className="col1">4.</td>
                                 <td className="col2">Ультразвуковое удаление наддесневых и поддесневых зубных отложений. Полость рта</td>
                                 <td className="col3">3500 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">5.</td>
+                                <td className="col1">4.</td>
                                 <td className="col2">Наложение временной пломбы</td>
                                 <td className="col3">700 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">6.</td>
+                                <td className="col1">5.</td>
                                 <td className="col2">Восстановление зуба пломбой I, V, VI класс по Блэку с использованием материалов из фотополимеров</td>
                                 <td className="col3">4200 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">7.</td>
+                                <td className="col1">6.</td>
                                 <td className="col2">Инструментальная и медикаментозная обработка корневого канала</td>
                                 <td className="col3">3000 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">8.</td>
+                                <td className="col1">7.</td>
                                 <td className="col2">Пломбирование корневого канала зуба</td>
                                 <td className="col3">3000 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">9.</td>
+                                <td className="col1">8.</td>
                                 <td className="col2">Диагностическое снятие пломбы</td>
                                 <td className="col3">1500 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">10.</td>
+                                <td className="col1">9.</td>
                                 <td className="col2">Восстановление зуба вкладками или виниром. Индивидуальная эстетика</td>
                                 <td className="col3">25000 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">11.</td>
+                                <td className="col1">10.</td>
                                 <td className="col2">Протезирование зубов полными съемными пластиночными протезами</td>
                                 <td className="col3">45000 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">12.</td>
+                                <td className="col1">11.</td>
                                 <td className="col2">Протезирование зуба с использованием имплантата Dentium (Корея), Paltop (Израиль).Коронка керамическая с титановым абатментом</td>
                                 <td className="col3">31900 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">13.</td>
+                                <td className="col1">12.</td>
                                 <td className="col2">Коррекция прикуса с использованием съемных и несъемных ортопедических конструкций. Релаксирующая каппа</td>
                                 <td className="col3">12000 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">14.</td>
+                                <td className="col1">13.</td>
                                 <td className="col2">Удаление постоянного зуба</td>
                                 <td className="col3">3500 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">15.</td>
+                                <td className="col1">14.</td>
                                 <td className="col2">Гингивопластика в области одного зуба</td>
                                 <td className="col3">8500 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">16.</td>
+                                <td className="col1">15.</td>
                                 <td className="col2">Пластика уздечки языка</td>
                                 <td className="col3">6500 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">17.</td>
+                                <td className="col1">16.</td>
                                 <td className="col2">Костная пластика челюстно-лицевой области с применением биодеградируемых материалов для увеличения толщины костного гребня</td>
                                 <td className="col3">49000 ₽</td>
                             </tr>
                             <tr>
-                                <td className="col1">18.</td>
+                                <td className="col1">17.</td>
                                 <td className="col2">Внутрикостная дентальная имплантация Implantium (Корея)</td>
                                 <td className="col3">29900 ₽</td>
                             </tr>
